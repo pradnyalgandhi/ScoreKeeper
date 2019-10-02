dc72f4703eae1a3d0bcfd22d039772ee7241ac6f
pradnyalgandhi <pradnyalgandhi28@gmail.com>
2019-10-03 01:30:44 +0530

    initial

--- app.js
+++ app.js
@@ -21,7 +21,34 @@ document.querySelector('.rollbutton').addEventListener('click', function(){
 	numDice.style.display ='block';
 	numDice.src = 'dice-' + dice + '.png';
 
+	if(dice !== 1){
+		roundScores += dice;
+		document.querySelector('#current' + activePlayer).textContent = roundScores;
+	}
+
+	else{
+			activePlayer === 1 ? activePlayer === 2 : activePlayer === 1;
+			roundScores = 0;
+			document.getElementById('current1').textContent ='0';
+			document.getElementById('current2').textContent ='0';
+
+			document.querySelector('.inner-div1').classList.toggle('active');
+			document.querySelector('.inner-div2').classList.toggle('active');
+
+			document.querySelector('.dice').style.display = 'none';
+
+		}
+	})
+
+document.querySelector('.holdbutton').addEventListener('click', function(){
+
+	document.querySelector('#score1').textContent = roundScores;
+	document.querySelector('#score1').style.display = roundScores;
+	document.getElementById('current' + activePlayer).textContent = 0;
+
 })
+
+
 
 
 
