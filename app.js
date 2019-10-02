1776e2df2aeac258dd9b5d93bd1fa6e7319ebf13
pradnyalgandhi <pradnyalgandhi28@gmail.com>
2019-10-02 22:47:22 +0530

    initial

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
+var dice, scores, roundScores, activePlayer;
+
+scores =[0,0];
+roundScores = 0;
+activePlayer = 1;
+
+document.getElementById('score1').textContent = '0';
+document.getElementById('score2').textContent = '0';
+document.getElementById('current1').textContent = '0';
+document.getElementById('current2').textContent = '0';
+
+dice = Math.floor(Math.random()*6) + 1;
+
+document.querySelector('.dice').style.display = 'none';
+
+document.querySelector('.rollbutton').addEventListener('click', function(){
+
+	dice = Math.floor(Math.random()*6) + 1;
+
+	var numDice = document.querySelector('.dice');
+	numDice.style.display ='block';
+	numDice.src = 'dice-' + dice + '.png';
+
+})
 
 
-var x = document.querySelector('#score1').textContent ;
-console.log(x);
+
+
+
